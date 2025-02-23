a30541dad9c457d6c9fc5edab02b69b1000141f2
GeraASM <gera2002h@gmail.com>
2025-02-22 18:49:55 -0600

    finish the program

---
+++ controlTV.py
@@ -0,0 +1,71 @@
+class ControlTV:
+    def __init__(self):
+        self.is_on = False
+        self.is_mute = False
+        self.volume = 15
+        self.volumeMAX = 20
+        self.channels = [range(0, 101)]
+        self.channel = 10
+
+    def turn_on(self):
+        self.is_on = True
+
+    def turn_off(self):
+        if not self.is_on:
+            return 
+        self.is_on = False
+    
+    def mute(self):
+        if not self.is_on:
+            return 
+        self.mute = True
+
+    def up_volume(self):
+        if not self.is_on:
+            return 
+        if self.is_mute:
+            self.is_mute = False
+        if self.volume > self.volumeMAX:
+            self.volume = self.volumeMAX
+            return self.volume
+        return self.volume + 1
+    
+    def low_volume(self):
+        if not self.is_on:
+            return
+        if self.is_mute:
+            self.is_mute = False
+        if self.volume <= 0:
+            self.volume = 0
+            self.is_mute = True
+            return self.volume
+        return self.volume - 1
+    
+    def up_channel(self):
+        if not self.is_on:
+            return
+        if self.channel > 100:
+            self.channel = 0
+            return self.channel
+        return self.channel + 1
+    
+    def low_channel(self):
+        if not self.is_on:
+            return
+        if self.channel < 0:
+            self.channel = 100
+            return self.channel
+        return self.channel - 1
+    
+    def find_channel(self, channel: int):
+        if not self.is_on:
+            return
+        if channel < 0 or channel > 100:
+            print(f"Channel {channel} not find")
+        if int(channel) in self.channel:
+            self.channel = channel
+        
+    def info(self):
+        if self.is_on:
+            info = f"""Mute: {'✅ On' if self.is_mute else '❌ Off'}\nVolume: {self.volume}\nChannel: {self.channel}"""
+            return info
---
+++ runprogram.py
@@ -0,0 +1,71 @@
+from controlTV import ControlTV
+import time
+
+menu = """Press 0️⃣ turn off\nPress 1️⃣ mute\nPress 2️⃣ up volume\nPress 3️⃣ low volumen\nPress 4️⃣ up channel\nPress 5️⃣ low channel\nPress 6️⃣ write de channel\nPress 7️⃣ get information\n Press 'q' to leave
+"""
+
+def main():
+    controls_list = []
+    mycontrol = ControlTV()
+
+    try:
+        while True:
+            # Aseguramos que la entrada sea explícita
+            user_input = input("Give '1' to turn on or '0' to turn off: ")
+            if user_input == '1':
+                mycontrol.turn_on()
+
+                print("################################")
+                print("Information control TV: \n")
+                print(mycontrol.info())
+                print("################################")
+
+                print(menu)
+
+                option = int(input("What do you want to do? "))
+
+                # Manejo del 'q' fuera del match
+                if option == 'q':
+                    print("Exiting...")
+                    break
+
+                match option:
+                    case 0:
+                        print("TV is turn OFF")
+                        mycontrol.turn_off()
+                        break
+                    case 1:
+                        print("TV is mute")
+                        mycontrol.mute()
+                    case 2:
+                        print("UP the volume")
+                        mycontrol.up_volume()
+                    case 3:
+                        print("LOW the volume")
+                        mycontrol.low_volume()
+                    case 4:
+                        print("UP channel")
+                        mycontrol.up_channel()
+                    case 5:
+                        print("LOW channel")
+                        mycontrol.low_channel()
+                    case 6:
+                        channel = int(input("What is the channel? "))
+                        mycontrol.find_channel(channel)
+                        print(f"Channel is {mycontrol.channel}\n")
+                    case 7:
+                        print(mycontrol.info())
+
+                    case _:
+                        print("Choose an option betweet 0 and 7\n")
+            else:
+                print("You need turn on the Control for use it!\n")
+                if mycontrol.is_on:
+                    mycontrol.turn_off()
+                time.sleep(2)
+
+    except KeyboardInterrupt:
+        print("Interruption with the keyboard")
+
+if __name__ == "__main__":
+    main()
